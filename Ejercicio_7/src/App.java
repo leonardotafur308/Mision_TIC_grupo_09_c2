8aec04799c25d8503976f7bfeae4dd23f48c578c
IsraelArbona <i.arbonammisena.edu.co>
2021-07-08 19:11:08 -0500

    Actualizacion 8 de julio del 2021

--- Ejercicio_7/src/App.java
+++ Ejercicio_7/src/App.java
@@ -1,7 +1,25 @@
-
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        // var sc = new 
+        // var sc = new
+        var sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero");
+        var numero = sc.nextInt();
+
+        var respuesta = validarNumero(numero);
+        System.out.println(respuesta);
     }
+
+    public static String validarNumero(int numero){
+        return (numero%2 == 0 ? numero + " Es par" : numero + " Es impar");
+        // return (numero%2 != 0 ? numero + " Es impar" : numero + " Es par");
+    }
+
+    /*
+        if ("condicion cumple"){
+            // respuesta
+        } else {
+            // respuesta
+        }
+    */
 }
